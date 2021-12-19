9b1cd10e30fedd62a8bbfd594765825a5a6d1b94
tehjul <sibillej@gmail.com>
2021-12-19 18:54:28 +0100

    main : added database creation

--- choozenREST/choozen/models.py
+++ choozenREST/choozen/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=50)
+    def __str__(self):
+        return self.username
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    year = models.IntegerField()
+    director = models.CharField(max_length=50)
+    rating = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.title + " (" + str(self.year) + ")"
--- choozenREST/choozenREST/settings.py
+++ choozenREST/choozenREST/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'choozen'
+    'choozen' # TODO : choozen.apps.ChoozenConfig ??
 ]
 
 MIDDLEWARE = [
