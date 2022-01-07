cc8a6f4c131833acc4b79be347ec8b4ac5970c50
tehjul <sibillej@gmail.com>
2022-01-07 11:10:51 +0100

    Added custom user model

--- choozenREST/choozen/models.py
+++ choozenREST/choozen/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     def __str__(self):
         return self.username
+
+class Users(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+      (CREATOR, 'Créateur'),
+      (SUBSCRIBER, 'Abonné'),
+    )
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
 
 class Movie(models.Model):
     title = models.CharField(max_length=50)
