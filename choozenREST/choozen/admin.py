e4ae1c6811f9a90e0484a72ea54a8a52f06c5a02
tehjul <sibillej@gmail.com>
2022-01-08 13:57:12 +0100

    Added test cases for movies

--- choozenREST/choozen/admin.py
+++ choozenREST/choozen/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Movie
 
-# Register your models here.
-admin.site.register(Movie)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+  list_display = ('title', 'year', 'genre', 'director', 'rating', 'id')
+  list_filter = ('title',)
+  search_fields = ('title',)
+  class Meta:
+    verbose_name_plural = 'Movies'
+    verbose_name = 'Movie'
--- choozenREST/choozen/tests.py
+++ choozenREST/choozen/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
-# Create your tests here.
+from choozen.models import Movie
+
+class MovieTestCase(TestCase):
+
+  DUMMY_MOVIE_TITLE = "The Matrix"
+
+  def setUp(self):
+    self.movie = Movie()
+    self.movie.title = self.DUMMY_MOVIE_TITLE
+    self.movie.year = 1999
+    self.movie.director = "Wachowski"
+    self.movie.rating = 8
+    self.movie.genre = "Sci-Fi"
+
+  def test_create_movie(self):
+    nb_of_movies_before = Movie.objects.count()
+    new_movie = self.movie
+    new_movie.save()
+    nb_of_movies_after = Movie.objects.count()
+    self.assertEqual(nb_of_movies_before + 1, nb_of_movies_after)
+    
+  def test_get_movie(self):
+    new_movie = self.movie
+    new_movie.save()
+    movie_from_db = Movie.objects.get(title=self.DUMMY_MOVIE_TITLE)
+    self.assertEqual(new_movie, movie_from_db)
+
+  def test_update_movie(self):
+    new_movie = self.movie
+    new_movie.save()
+    movie_from_db = Movie.objects.get(title=self.DUMMY_MOVIE_TITLE)
+    movie_from_db.title = "The Matrix Reloaded"
+    movie_from_db.save()
+    self.assertEqual(movie_from_db.title, "The Matrix Reloaded")
+
+  def test_delete_movie(self):
+    new_movie = self.movie
+    new_movie.save()
+    movie_from_db = Movie.objects.get(title=self.DUMMY_MOVIE_TITLE)
+    movie_from_db.delete()
+    self.assertEqual(Movie.objects.count(), 0)
