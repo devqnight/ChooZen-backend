deb32c4862edc609dd911fbb6edceb9dd5f74de4
tehjul <sibillej@gmail.com>
2022-01-12 14:36:17 +0100

    Add test cases for User

--- choozenREST/choozen/tests.py
+++ choozenREST/choozen/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from choozen.models import Movie
+from choozen.models import Movie, User
 
 class MovieTestCase(TestCase):
 
@@ -42,3 +42,43 @@ class MovieTestCase(TestCase):
     movie_from_db = Movie.objects.get(title=self.DUMMY_MOVIE_TITLE)
     movie_from_db.delete()
     self.assertEqual(Movie.objects.count(), 0)
+
+class UserTestCase(TestCase):
+
+    DUMMY_USER_NAME = "John"
+    UPDATE_USER_NAME = "Quentin"
+  
+    def setUp(self):
+      self.user = User()
+      self.user.username = self.DUMMY_USER_NAME
+      self.user.email = "testuser@testage.com"
+      self.user.password = "testpassword"
+      self.user.birthdate = "1999-01-01"
+
+    def test_create_user(self):
+      nb_of_users_before = User.objects.count()
+      new_user = self.user
+      new_user.save()
+      nb_of_users_after = User.objects.count()
+      self.assertEqual(nb_of_users_before + 1, nb_of_users_after)
+
+    def test_get_user(self):
+      new_user = self.user
+      new_user.save()
+      user_from_db = User.objects.get(username=self.DUMMY_USER_NAME)
+      self.assertEqual(new_user, user_from_db)
+
+    def test_update_user(self):
+      new_user = self.user
+      new_user.save()
+      user_from_db = User.objects.get(username=self.DUMMY_USER_NAME)
+      user_from_db.username = self.UPDATE_USER_NAME
+      user_from_db.save()
+      self.assertEqual(user_from_db.username, self.UPDATE_USER_NAME)
+
+    def test_delete_user(self):
+      new_user = self.user
+      new_user.save()
+      user_from_db = User.objects.get(username=self.DUMMY_USER_NAME)
+      user_from_db.delete()
+      self.assertEqual(User.objects.count(), 0)
