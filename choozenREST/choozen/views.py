09095ead8a98619cd618606e12fe270e61580d44
tehjul <sibillej@gmail.com>
2022-01-31 16:29:42 +0100

    Add user informations on is_authenticated response

--- choozenREST/choozen/views.py
+++ choozenREST/choozen/views.py
@@ -1,6 +1,8 @@
+from django.http import JsonResponse
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.middleware.csrf import get_token
+from django.core import serializers
 from choozenREST.imdb import search_movie_by_title
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authtoken.models import Token
@@ -35,7 +37,19 @@ def is_authenticated(request):
         user = User.objects.get(username=username)
         token_db = Token.objects.get(key=token)
         if user.id == token_db.user_id:
-          return HttpResponse(True, content_type='application/json', status=200)
+          data = {'authenticated': True}
+          data['id'] = user.id
+          data['username'] = user.username
+          data['first_name'] = user.first_name
+          data['last_name'] = user.last_name
+          data['email'] = user.email
+          data['birthdate'] = user.birthdate
+          data['last_login'] = user.last_login
+          data['date_joined'] = user.date_joined
+          data['is_superuser'] = user.is_superuser
+          data['is_staff'] = user.is_staff
+          data['is_active'] = user.is_active
+          return JsonResponse(data, content_type='application/json', safe=False, status= 203)
         else:
           return HttpResponse(False, content_type='application/json', status=401)
       except (User.DoesNotExist, Token.DoesNotExist):
