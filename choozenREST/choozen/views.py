0ab75cdd55bbabec5a65dde5eabceb5a9e5b98d2
tehjul <sibillej@gmail.com>
2022-01-31 14:22:49 +0100

    Updated is_authenticated to check username with token

--- choozenREST/choozen/views.py
+++ choozenREST/choozen/views.py
@@ -3,8 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.middleware.csrf import get_token
 from choozenREST.imdb import search_movie_by_title
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authtoken.models import Token
 
-from .models import Movie
+from .models import Movie, User
 from choozenREST.serializers import MovieSerializer
 
 class MovieViewSet(ModelViewSet):
@@ -27,7 +28,17 @@ def get_csrf(request):
     return HttpResponse(token)
 
 def is_authenticated(request):
-    if request.user.is_authenticated:
-        return HttpResponse(True, content_type='application/json', status=200)
+    if request.method == 'POST':
+      username = request.POST.get('username')
+      token = request.POST.get('token')
+      try:
+        user = User.objects.get(username=username)
+        token_db = Token.objects.get(user=user)
+        if token == token_db.key:
+          return HttpResponse(True, content_type='application/json', status=200)
+        else:
+          return HttpResponse(False, content_type='application/json', status=401)
+      except (User.DoesNotExist, Token.DoesNotExist):
+        return HttpResponse("The user or token does not exist", content_type='application/json', status=401)
     else:
-        return HttpResponse(False, content_type='application/json', status=401)
+      return HttpResponse("Only POST requests are allowed", content_type='application/json', status=405)
